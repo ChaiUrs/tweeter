600d9c1acfe84daf395a70effcafdc1320f6e8fb
ChaiUrs <mysorechaitra@gmail.com>
2019-08-15 08:07:13 +0000

    Created a scroll up form toggle, updated other CSS files and index.html

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,5 @@
+
+"use strict";
 /*
  * Client-side JS logic goes here
  * jQuery is already loaded
@@ -5,49 +7,59 @@
  */
 
 $(document).ready(function() {
-  // Fake data taken from initial-tweets.json
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
+
+//adapted from https://stackoverflow.com/
+  const timeDifference = function(current, previous) {
+    const msPerMinute = 60 * 1000;
+    const msPerHour = msPerMinute * 60;
+    const msPerDay = msPerHour * 24;
+    const msPerMonth = msPerDay * 30;
+    const msPerYear = msPerDay * 365;
+  
+    const elapsed = current - previous;
+    let plural = 's';
+  
+    if (elapsed < msPerMinute) {
+      const seconds = Math.round(elapsed / 1000);
+      if (seconds === 1) plural = '';
+      return seconds + ' second' + plural + ' ago';
+    } else if (elapsed < msPerHour) {
+      const minutes = Math.round(elapsed / msPerMinute);
+      if (minutes === 1) plural = '';
+      return minutes + ' minute' + plural + ' ago';
+    } else if (elapsed < msPerDay) {
+      const hours = Math.round(elapsed / msPerHour);
+      if (hours === 1) plural = '';
+      return hours + ' hour' + plural + ' ago';
+    } else if (elapsed < msPerMonth) {
+      const days = Math.round(elapsed / msPerDay);
+      if (days === 1) plural = '';
+      return days + ' day' + plural + ' ago';
+    } else if (elapsed < msPerYear) {
+      const months = Math.round(elapsed / msPerMonth);
+      if (months === 1) plural = '';
+      return months + ' month' + plural + ' ago';
+    } else {
+      const years = Math.round(elapsed / msPerYear);
+      if (years === 1) plural = '';
+      return years + ' year' + plural + ' ago';
     }
-  ];
+  };
   
-  // const daysAgo = function(time) {
-  //   return new Date(time).getDate() - (new Date).getDate();
-  // };
   
-  const createTweetElement = function(tweetObj) {
+  const createTweetElement = function(tweetsObj) {
     return `
     <article class="tweets-container">
       <header>
-      <img class="header-image" src="${tweetObj.user.avatars}">
-        <span class="header-name">${tweetObj.user.name}</span>
-        <span class="handler-text">${tweetObj.user.handle}</span>
+      <img class="header-image" src="${tweetsObj.user.avatars}">
+        <span class="header-name">${tweetsObj.user.name}</span>
+        <span class="handler-text">${tweetsObj.user.handle}</span>
       </header>
       <main class="main-text">
-        ${tweetObj.content.text}
+        ${tweetsObj.content.text}
       </main>
       <footer class="tweet-footer">
-        <span>${tweetObj.created_at}</span>
+        <span class="days-ago">${timeDifference(Date.now(), tweetsObj.created_at)}</span>
         <span class="tweet-icons">
         <i class="fas fa-flag"></i>
         <i class="fas fa-retweet"></i>
@@ -57,19 +69,68 @@ $(document).ready(function() {
     </article>
     `;
   };
-    
-const renderTweets = function(tweets) {
-  // $("#tweets-container").empty(); //clear the container before to read all tweets
-  for(const tweetEle of tweets) {   //loops through tweets
-    //console.log(tweets[tweetEle]);
+  
+  const escape =  function(str) {
+    let div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  }
 
-    //calls createTweetElement for each tweet
-    //takes return value and appends it to the tweets container
-    $("#tweets-container").append(createTweetElement(tweetEle)); 
+  
+  const renderTweets = function(tweets) {
+    $("#tweets-container").empty(); //clear the container before to read all tweets
+    for(const tweetEle of tweets) {   //loops through tweets
+      //console.log(tweets[tweetEle]);
+      //calls createTweetElement for each tweet
+      //takes return value and appends it to the tweets container
+      $("#tweets-container").append(createTweetElement(tweetEle));
+    }
   };
-}
+  
 
-renderTweets(data);
+  //this function will use jQuery to make a request to /tweets and receive the array of tweets as JSON.
+  const loadTweets = function() {
+    return $.ajax({
+      url: "/tweets/",
+      type: "GET",
+    })
+  }
+  
 
+  $("section.new-tweet > form").on("submit", function(event) {
+    event.preventDefault(); // to prevent the default form submission behaviour
+    
+    //Form Validation
+    const $tweetCount = $("section.new-tweet > form > textarea.form-submit")
+    if ($tweetCount.val() === null || $tweetCount.val() === "") {
+      alert("ERROR: This field cannot be empty! Please input some text.");
+    } else if ($tweetCount.val().length > 140) {
+      alert("Ho Ho Ho! Your message is way too much!!!");
+    } else {
+      $.ajax({
+        url: "/tweets/",
+        type: "POST",
+        data: $(this).serialize()
+      })
+      .then(() => {
+        refreshTweets();
+      })
+    }
+  })
+
+
+  //Expands new tweet form when scroll down tweet button is clicked
+  $(".composeButton").on("click", function() {
+    $(".new-tweet").slideToggle();
+    $("textarea").focus();
+  });
+
+   //Load and Render tweets
+   const refreshTweets = function() {
+    loadTweets()
+    .then(renderTweets)
+    .fail(error => console.log(error.statusText));
+  }
+  refreshTweets();
 });
 
